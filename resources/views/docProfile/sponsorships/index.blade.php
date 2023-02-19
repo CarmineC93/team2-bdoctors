62bd006ca15fe532f320b376eb10fe4aab90f144
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-02-19 16:37:01 +0100

    ADD: page plugin

--- resources/views/docProfile/sponsorships/index.blade.php
+++ resources/views/docProfile/sponsorships/index.blade.php
@@ -7,11 +7,11 @@
         @foreach ($promos as $promo)
             <div>
                 <h2>{{ $promo->name }}</h2>
-                <p>{{ $promo->price }}</p>
+                <p>{{ $promo->price }} Euro</p>
 
-                <p>{{ $promo->duration }}</p>
+                <p>{{ $promo->duration }} h</p>
                 <button>
-                    <a href="{{ route('promos.show', $promo->id) }}"> Acquista </a>
+                    <a href="{{ route('promos.show', $promo->id) }}"> Dettagli </a>
                 </button>
             </div>
         @endforeach
